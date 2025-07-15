e93b1044856949392c52b1c6d4c34b214f24b319
nmeusling <15682036+nmeusling@users.noreply.github.com>
2025-07-15 13:23:30 -0400

    chore: set up repo

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
-def main():
-    print("Hello from vote!")
+import typer
+from rich import print
+from typing_extensions import Annotated
+
+app = typer.Typer(no_args_is_help=True)
+
+
+@app.command()
+def create(election_name: Annotated[str, typer.Option(prompt=True)]):
+    print("Create an election")
+    if election_name:
+        print(f"Created {election_name}")
+    else:
+        print("Please enter election name: ")
+
+
+@app.command()
+def view():
+    print("View and election")
 
 
 if __name__ == "__main__":
-    main()
+    app()
